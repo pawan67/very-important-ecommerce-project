69938d3fde943d970829803de52508e930e8d3a4
Pawan <pawantamada8@gmail.com>
2022-10-31 20:58:17 +0530

    added very important function

--- app/script.js
+++ app/script.js
@@ -3,6 +3,13 @@ const ProductVideo = document.getElementById("product-video");
 const showProducts = () => {
   ProductVideo.style.top = "50%";
   ProductVideo.play();
+
+  // run the function after 5 seconds
+  setTimeout(redirectToProductPage, 5000);
+};
+
+const redirectToProductPage = () => {
+  window.location.href = "https://www.youtube.com/watch?v=GtL1huin9EE";
 };
 
 const stop = () => {
